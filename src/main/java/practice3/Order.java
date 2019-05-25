e2b8c3d5e929e7a656600bf47cb8b6faa5b8fe7c
huyf <spyssor@gmail.com>
2019-05-25 14:54:06 +0800

    完成practice3：使用抽象方法对象重构遗留代码

--- src/main/java/practice3/Order.java
+++ src/main/java/practice3/Order.java
@@ -3,37 +3,12 @@ package practice3;
 import java.math.BigDecimal;
 import java.util.List;
 
-public class Order {
-
-    private List<OrderLineItem> orderLineItemList;
-    private List<BigDecimal> discounts;
-    private BigDecimal tax;
+public class Order extends PriceCaculator{
 
     public Order(List<OrderLineItem> orderLineItemList, List<BigDecimal> discounts) {
-        this.orderLineItemList = orderLineItemList;
-        this.discounts = discounts;
-        this.tax = new BigDecimal(0.1);
+        super.orderLineItemList = orderLineItemList;
+        super.discounts = discounts;
+        super.tax = new BigDecimal(0.1);
     }
 
-    public BigDecimal calculate() {
-        BigDecimal subTotal = new BigDecimal(0);
-
-        // Total up line items
-        for (OrderLineItem lineItem : orderLineItemList) {
-            subTotal = subTotal.add(lineItem.getPrice());
-        }
-
-        // Subtract discounts
-        for (BigDecimal discount : discounts) {
-            subTotal = subTotal.subtract(discount);
-        }
-
-        // calculate tax
-        BigDecimal tax = subTotal.multiply(this.tax);
-
-        // calculate GrandTotal
-        BigDecimal grandTotal = subTotal.add(tax);
-
-        return grandTotal;
-    }
 }
--- src/main/java/practice3/PriceCaculator.java
+++ src/main/java/practice3/PriceCaculator.java
@@ -1,4 +1,33 @@
 package practice3;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class PriceCaculator {
+
+    protected List<OrderLineItem> orderLineItemList;
+    protected List<BigDecimal> discounts;
+    protected BigDecimal tax;
+
+    public BigDecimal calculate() {
+        BigDecimal subTotal = new BigDecimal(0);
+
+        // Total up line items
+        for (OrderLineItem lineItem : orderLineItemList) {
+            subTotal = subTotal.add(lineItem.getPrice());
+        }
+
+        // Subtract discounts
+        for (BigDecimal discount : discounts) {
+            subTotal = subTotal.subtract(discount);
+        }
+
+        // calculate tax
+        BigDecimal tax = subTotal.multiply(this.tax);
+
+        // calculate GrandTotal
+        BigDecimal grandTotal = subTotal.add(tax);
+
+        return grandTotal;
+    }
 }
